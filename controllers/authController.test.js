238b9ba6f48b0c27d3a17dcdd50ff794fac48ec3
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2026-02-12 21:23:45 +0800

    Add unit tests for getAllOrdersController

--- controllers/authController.test.js
+++ controllers/authController.test.js
@@ -1,5 +1,5 @@
 import orderModel from "../models/orderModel.js";
-import { getOrdersController } from "../controllers/authController.js";
+import { getOrdersController, getAllOrdersController } from "../controllers/authController.js";
 
 jest.mock("../models/orderModel.js");
 
@@ -69,6 +69,78 @@ describe("Tests for getOrdersController", () => {
 
         // Assert
         expect(orderModel.find).toHaveBeenCalledWith({ buyer: "123" });
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledWith({
+            success: false,
+            message: "Error while getting orders",
+            error,
+        });
+    });
+});
+
+describe("Tests for getAllOrdersController", () => {
+
+    // Set up variables for our test cases
+    let req, res;
+
+    beforeEach(() => {
+        req = {};
+        res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+            send: jest.fn(),
+        };
+        jest.clearAllMocks();
+    });
+
+    test("Return 200 after fetching all orders successfully", async () => {
+        /* 
+        Assumption: 
+        - getAllOrdersController should return a 200 once the orders has been retrieved successfully.
+        */
+        // Arrange
+        // Assume all this is in sorted order of creation time
+        const mockOrders = [
+            { _id: "1", buyer: "user123", products: [], payment: {}, status: "Not Process" },
+            { _id: "2", buyer: "user123", products: [], payment: {}, status: "Shipped" },
+            { _id: "3", buyer: "user321", products: [], payment: {}, status: "Processing" },
+            { _id: "4", buyer: "user4", products: [], payment: {}, status: "Shipped" },
+        ];
+
+        const mockQuery = {
+            populate: jest.fn().mockReturnThis(),
+            sort: jest.fn().mockReturnValue(mockOrders)
+        };
+
+        orderModel.find.mockReturnValue(mockQuery);
+
+        // Act
+        await getAllOrdersController(req, res);
+
+        // Assert
+        expect(orderModel.find).toHaveBeenCalledWith({});
+        expect(mockQuery.populate).toHaveBeenCalledWith("products", "-photo");
+        expect(mockQuery.populate).toHaveBeenCalledWith("buyer", "name");
+        expect(mockQuery.sort).toHaveBeenCalledWith({ createdAt: "-1" });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(mockOrders);
+    });
+
+    test("Return 500 when an error occurs while fetching orders", async () => {
+        /* 
+        Assumption: MongoDB will throw an error when the buyer id is invalid.
+        */
+        // Arrange
+        const error = new Error("Database error");
+        orderModel.find.mockImplementation(() => {
+            throw error;
+        });
+
+        // Act
+        await getAllOrdersController(req, res);
+
+        // Assert
+        expect(orderModel.find).toHaveBeenCalledWith({});
         expect(res.status).toHaveBeenCalledWith(500);
         expect(res.send).toHaveBeenCalledWith({
             success: false,
