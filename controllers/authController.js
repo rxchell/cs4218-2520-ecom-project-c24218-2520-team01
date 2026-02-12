6cd1ca02d854d7a173eaefa507576c5b16b2b80c
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2026-02-12 21:10:13 +0800

    Fix bugs in getOrdersController

--- controllers/authController.js
+++ controllers/authController.js
@@ -206,12 +206,12 @@ export const getOrdersController = async (req, res) => {
       .find({ buyer: req.user._id })
       .populate("products", "-photo")
       .populate("buyer", "name");
-    res.json(orders);
+    res.status(200).json(orders);
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Error WHile Geting Orders",
+      message: "Error while getting orders",
       error,
     });
   }
