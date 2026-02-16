b3c793b491b011a4b968c6fcb1489c2cd516758c
ITLimJiaWei <jiaweilim77@gmail.com>
2026-02-16 21:43:37 +0800

    Add admin test file to config

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,14 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/admin/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/pages/admin/**"],
   coverageThreshold: {
     global: {
       lines: 100,
