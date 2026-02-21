e3ed30a307fb84dadcbbf7201bf91578083b4546
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2026-02-21 11:15:48 +0800

    Clean up unit tests for Categories.js

--- client/src/pages/Categories.test.js
+++ client/src/pages/Categories.test.js
@@ -1,3 +1,4 @@
+import { beforeAll, beforeEach, describe, expect, jest } from "@jest/globals";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
@@ -8,73 +9,89 @@ import Layout from "../components/Layout";
 // Mock the layout and useCategory hook
 jest.mock("../components/Layout", () => {
     return ({ children }) => (
-        <div data-testid="mock layout">
+        <div data-testid="Mock layout">
             {children}
         </div>
     );
 });
 jest.mock("../hooks/useCategory");
 
-const renderComponent = () =>
-    render(
-        <MemoryRouter>
-            <Categories />
-        </MemoryRouter>
-    );
+describe("Frontend UI for categories page", () => {
+    describe("Unit tests for categories UI page", () => {
 
+        let renderComponent;
 
-describe("Unit test for categories UI component", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+        beforeAll(() => {
+            // We will use this function variable to render the component
+            // MemoryRouter is used to store the links in memory
+            renderComponent = () =>
+                render(
+                    <MemoryRouter>
+                        <Categories />
+                    </MemoryRouter>
+                );
+        })
 
-    test("Render successfully with no categories", () => {
-        // Arrange
-        useCategory.mockReturnValue([]);
-
-        // Act
-        renderComponent();
-
-        // Assert
-        expect(screen.getByTestId("mock layout")).toBeInTheDocument(); // Ensures we have rendered the page
-        const links = screen.queryAllByRole("link"); // Verify no category links are rendered
-        expect(links).toHaveLength(0);
-    });
-
-    test("Renders categories after being fetched by useCategory hook", () => {
-        // Arrange
-        const mockCategories = [
-            { _id: 1, name: "Electronics", slug: "electronics" },
-            { _id: 2, name: "Books", slug: "books" },
-        ];
-        useCategory.mockReturnValueOnce(mockCategories);
-
-        // Act
-        renderComponent();
-
-        // Assert
-        expect(screen.getByText("Electronics")).toBeInTheDocument();
-        expect(screen.getByText("Books")).toBeInTheDocument();
-    });
-
-    test("Renders correct links on categories page", () => {
-        // Arrange
-        const mockCategories = [
-            { _id: 1, name: "Electronics", slug: "electronics" },
-            { _id: 2, name: "Books", slug: "books" },
-        ];
-        useCategory.mockReturnValueOnce(mockCategories);
-
-        // Act
-        renderComponent();
-
-        // Assert
-        const links = screen.getAllByRole("link");
-        expect(links).toHaveLength(mockCategories.length);
-
-        mockCategories.forEach((category, index) => {
-            expect(links[index]).toHaveTextContent(category.name);
-            expect(links[index]).toHaveAttribute("href", `/category/${category.slug}`);
+        beforeEach(() => {
+            jest.clearAllMocks();
         });
-    });
+
+        describe("Successfully renders the correct elements on the page", () => {
+            test("Renders categories after being fetched by useCategory hook", () => {
+                // Arrange
+                const mockCategoriesList = [
+                    { _id: 1, name: "Electronics", slug: "electronics" },
+                    { _id: 2, name: "Books", slug: "books" },
+                ];
+                useCategory.mockReturnValueOnce(mockCategoriesList);
+
+                // Act
+                renderComponent();
+
+                // Assert
+                expect(screen.getByText("Electronics")).toBeInTheDocument();
+                expect(screen.getByText("Books")).toBeInTheDocument();
+            });
+
+            test("Render successfully with no categories fetched", () => {
+                // Arrange
+                useCategory.mockReturnValue([]);
+
+                // Act
+                renderComponent();
+
+                // Assert
+                // Ensures we have rendered the page
+                expect(screen.getByTestId("Mock layout")).toBeInTheDocument();
+                // Verify no category links are rendered
+                const links = screen.queryAllByRole("link");
+                expect(links).toHaveLength(0);
+            });
+
+            test("Renders correct links on categories page", () => {
+                // Arrange
+                const mockCategories = [
+                    { _id: 1, name: "Electronics", slug: "electronics" },
+                    { _id: 2, name: "Books", slug: "books" },
+                ];
+                useCategory.mockReturnValueOnce(mockCategories);
+
+                // Act
+                renderComponent();
+
+                // Assert
+                // Just check again that the categories are rendered
+                expect(screen.getByText("Electronics")).toBeInTheDocument();
+                expect(screen.getByText("Books")).toBeInTheDocument();
+
+                // Check that the links are rendered correctly
+                const links = screen.getAllByRole("link");
+                expect(links).toHaveLength(mockCategories.length);
+                mockCategories.forEach((category, index) => {
+                    expect(links[index]).toHaveTextContent(category.name);
+                    expect(links[index]).toHaveAttribute("href", `/category/${category.slug}`);
+                });
+            });
+        })
+    })
 })
